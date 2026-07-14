e853b67050aadbd7e49481f5ca4fbab3d4b5bed5
jmpnm84 <jmpnm84@gmail.com>
2026-07-14 07:55:54 +0100

    Update service worker to cache manifest.json

--- sw.js
+++ sw.js
@@ -1,21 +1,17 @@
 const CACHE_NAME = 'trigo-v17';
-const ASSETS = [
-    './',
-    './index.html',
-    'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'
-];
-
 self.addEventListener('install', e => {
     e.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+        caches.open(CACHE_NAME).then(cache => cache.addAll([
+            './',
+            './index.html',
+            './manifest.json'
+        ]))
         .then(() => self.skipWaiting())
     );
 });
-
 self.addEventListener('activate', e => {
     e.waitUntil(self.clients.claim());
 });
-
 self.addEventListener('fetch', e => {
     e.respondWith(
         caches.match(e.request).then(res => res || fetch(e.request))
